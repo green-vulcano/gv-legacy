bc05c2e26ae3db6001a0f23ebd1612bf98f04d2e
Aretus <m.sutera@greenvulcano.com>
2017-02-10 16:35:14 +0100

    update POPCallOperationTest

---
+++ gvlegacy/gvvcl-mail/src/test/java/it/greenvulcano/gvesb/virtual/imap/IMAPCallOperationTest.java
@@ -0,0 +1,14 @@
+package it.greenvulcano.gvesb.virtual.imap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IMAPCallOperationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
